dd8f64e748f5f9f711a8dccdd1a1d3fc969538ce
Zachary Lapointe <Zachary.lapointe@mail.concordia.ca>
2018-09-19 16:26:45 -0400

    Made Debug.log() more efficient

--- src/com/zalinius/utilities/Debug.java
+++ src/com/zalinius/utilities/Debug.java
@@ -3,7 +3,7 @@ package com.zalinius.utilities;
 public class Debug {
 	
 	public static void log(String message) {
-		System.out.println(message);
+		System.out.print(message + "\n");
 	}
 	
 	public static void error(String error) {
