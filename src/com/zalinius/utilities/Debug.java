b9b291c90c1996205ca58108d12de33e46ae62d2
Zachary Lapointe <Zachary.lapointe@mail.concordia.ca>
2018-09-19 16:21:22 -0400

    Added Debug.log()

---
+++ src/com/zalinius/utilities/Debug.java
@@ -0,0 +1,13 @@
+package com.zalinius.utilities;
+
+public class Debug {
+	
+	public static void log(String message) {
+		System.out.println(message);
+	}
+	
+	public static void error(String error) {
+		System.err.println(error);
+	}
+
+}
