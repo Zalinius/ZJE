c2abeeab47a3659bbdaa0ae9941e04e79bb3f681
Zachary Lapointe <Zachary.lapointe@mail.concordia.ca>
2018-11-21 17:14:47 -0500

    Added some comments, xClamp, and some tests

--- src/com/zalinius/utilities/ZMath.java
+++ src/com/zalinius/utilities/ZMath.java
@@ -4,36 +4,90 @@ import com.zalinius.physics.Point2D;
 
 public class ZMath {
 
-    public static double clamp(double value, double leftBoundary, double rightBoundary){
+	/**
+	 * Constrains a value between two bounds (inclusive).
+	 * The bounds need not be ordered.
+	 * @return The value if it is within the two values, else the closest bound to the value.
+	 */
+    public static double clamp(double value, double bound1, double bound2){
         double clampedValue = value;
-        double realLeftBound = leftBoundary;
-        double realRightBound = rightBoundary;
+        double leftBound = bound1;
+        double rightBound = bound2;
         
-        if(rightBoundary < leftBoundary) {
-        	realLeftBound = rightBoundary;
-        	realRightBound = leftBoundary;
+        if(bound2 < bound1) {
+        	leftBound = bound2;
+        	rightBound = bound1;
         }
         
-        if(value < realLeftBound) {
-        	clampedValue = realLeftBound;
+        if(value < leftBound) {
+        	clampedValue = leftBound;
         }
-        else if(value > realRightBound) {
-        	clampedValue = realRightBound;
+        else if(value > rightBound) {
+        	clampedValue = rightBound;
         }
 
         return clampedValue;
     }
     
+	/**
+	 * Constrains a value between two values (inclusive).
+	 * The bounds need not be ordered.
+	 * @return The value if it is within the two values, else the closest bound to the value.
+	 */
     public static int clamp(int value, int leftBound, int rightBound) {
     	return (int)clamp((double)value, leftBound, rightBound);
     }
     
-    public static Point2D clamp(Point2D point, Point2D leftbound, Point2D rightBound) {
-    	double clampedX = clamp(point.x, leftbound.x, rightBound.x);
-    	double clampedY = clamp(point.y, leftbound.y, rightBound.y);
+	/**
+	 * Constrains a value to be outside of two bounds (inclusive).
+	 * The bounds need not be ordered.
+	 * @return The value if it is within the two values, else the closest bound to the value.
+	 */
+    public static double xClamp(double value, double bound1, double bound2) {
+        double clampedValue = value;
+        double leftBound = bound1;
+        double rightBound = bound2;
+        
+        if(bound2 < bound1) {
+        	leftBound = bound2;
+        	rightBound = bound1;
+        }
+        
+        if(value > leftBound && value < rightBound) {
+        	double midPoint = (leftBound + rightBound) / 2;
+        	
+        	if(value < midPoint) {
+        		clampedValue = leftBound;
+        	}
+        	else if(value > midPoint) {
+        		clampedValue = rightBound;
+        	}
+        	else {
+        		throw new RuntimeException("Not implemented toooo confusing");
+        		//TODO what do? maybe pick randomly? maybe have one be favored?
+        	}
+        }
+
+        return clampedValue;
+    }
+    
+	/**
+	 * Constrains a value to be outside of two bounds (inclusive).
+	 * The bounds need not be ordered.
+	 * @return The value if it is within the two values, else the closest bound to the value.
+	 */
+    public static int xClamp(int value, int bound1, int bound2) {
+    	return (int) xClamp((double)value, bound1, bound2);
+    }
+    
+
+    public static Point2D clamp(Point2D point, Point2D bound1, Point2D bound2) {
+    	double clampedX = clamp(point.x, bound1.x, bound2.x);
+    	double clampedY = clamp(point.y, bound1.y, bound2.y);
     	
     	return new Point2D(clampedX, clampedY);
     }
+    
     
     public static boolean isBetween(double left, double right, double x) {
     	if(left == right) {
--- test/com/zalinius/utilities/ZMathTest.java
+++ test/com/zalinius/utilities/ZMathTest.java
@@ -7,8 +7,108 @@ import org.junit.jupiter.api.Test;
 public class ZMathTest {
 
 	@Test
-	void clampTest() {
-		assertEquals(10, ZMath.clamp(10, 5, 15));
+	void doubleClamp_inside_isUnchanged() {
+		double value = 5.2;
+		double left = 4.5;
+		double right = 6.2;
+		
+		double result = ZMath.clamp(value, left, right);
+				
+		assertEquals(value, result);	}
+	
+	@Test
+	void doubleClamp_onBound_isUnchanged() {
+		double value = 3.2;
+		double left = value;
+		double right = 6.2;
+		
+		double result = ZMath.clamp(value, left, right);
+				
+		assertEquals(value, result);	}
+	
+	@Test
+	void doubleClamp_outside_isClamped() {
+		double value = 3.2;
+		double left = 4.5;
+		double right = 6.2;
+		
+		double result = ZMath.clamp(value, left, right);
+				
+		assertEquals(left, result);
+	}
+	
+	@Test
+	void intClamp_inside_isUnchanged() {
+		int value = 5;
+		int left = 4;
+		int right = 6;
+		
+		int result = ZMath.clamp(value, left, right);
+				
+		assertEquals(value, result);	}
+	
+	@Test
+	void intClamp_onLowerBound_isUnchanged() {
+		int value = 3;
+		int left = value;
+		int right = 6;
+		
+		int result = ZMath.clamp(value, left, right);
+				
+		assertEquals(value, result);
+	}
+	
+	@Test
+	void intClamp_onUpperBound_isUnchanged() {
+		int value = 6;
+		int left = 3;
+		int right = value;
+		
+		int result = ZMath.clamp(value, left, right);
+				
+		assertEquals(value, result);
+	}
+	
+	@Test
+	void intClamp_outside_isClamped() {
+		int value = 3;
+		int left = 4;
+		int right = 6;
+		
+		int result = ZMath.clamp(value, left, right);
+				
+		assertEquals(left, result);
+	}
+	
+	@Test
+	void doubleXClamp_inside_pushedLeft() {
+		double value = 5.2;
+		double left = 4.5;
+		double right = 6.2;
+		
+		double result = ZMath.xClamp(value, left, right);
+				
+		assertEquals(left, result);	}
+	
+	@Test
+	void doubleXClamp_onBound_isUnchanged() {
+		double value = 3.2;
+		double left = value;
+		double right = 6.2;
+		
+		double result = ZMath.xClamp(value, left, right);
+				
+		assertEquals(value, result);	}
+	
+	@Test
+	void doubleXClamp_outside_isUnchanged() {
+		double value = 3.2;
+		double left = 4.5;
+		double right = 6.2;
+		
+		double result = ZMath.xClamp(value, left, right);
+				
+		assertEquals(value, result);
 	}
 
 }
