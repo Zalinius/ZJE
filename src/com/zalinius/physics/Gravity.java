6cc0f1b70bfa9a324605652c26e0adc27389296d
Zachary Lapointe <Zachary.lapointe@mail.concordia.ca>
2020-03-18 21:47:19 -0400

    Gravity Y-inversion

--- src/com/zalinius/physics/Gravity.java
+++ src/com/zalinius/physics/Gravity.java
@@ -1,7 +1,7 @@
 package com.zalinius.physics;
 
 public class Gravity {
-	public static final double GRAVITY = -9.81;
+	public static final double GRAVITY = 9.81;
 	
 	public static Vector2D fall(double delta, Moveable object) {
 		Vector2D v0 = object.velocity();
