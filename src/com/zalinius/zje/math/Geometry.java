3162e1f76812c14ccc0c8e09d0438e5a6dc79049
Zachary Lapointe <lapointez@hotmail.ca>
2021-07-13 16:44:52 -0400

    Added a couple simple geometry functions from other projects

---
+++ src/com/zalinius/zje/math/Geometry.java
@@ -0,0 +1,42 @@
+package com.zalinius.zje.math;
+
+import java.awt.Shape;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.zalinius.zje.physics.Point;
+
+public class Geometry {
+	
+	public static Shape makePolygonShape(List<Point> points) {
+		Path2D.Double path = new Path2D.Double();
+		
+		Iterator<Point> it = points.iterator();
+		Point firstPoint = it.next();
+		path.moveTo(firstPoint.x, firstPoint.y);
+		while (it.hasNext()) {
+			Point point = it.next();
+			path.lineTo(point.x, point.y);
+		}
+		path.closePath();
+		
+		return path;
+	}
+
+	public static List<Point> regularPolygon(Point center, int sides, double radius) {
+		List<Point> points = new ArrayList<>();
+		sides = Math.max(sides, 3);
+		
+		for(int i = 0; i != sides; ++i) {
+			double angle = (2*Math.PI) * ((i+.5) / sides);
+			double x = radius * Math.cos(angle) + center.x;
+			double y = radius * Math.sin(angle) + center.y;
+			
+			points.add(new Point(x, y));
+		}
+		
+		return points;
+	}
+}
