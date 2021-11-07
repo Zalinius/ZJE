018ef2b4657608c07c870ba8c6d29ab9e999b0c1
Zachary Lapointe <lapointez@hotmail.ca>
2021-11-07 09:51:12 -0500

    Bug fixes and dpad support: - null bufferDimensions fix
    - Added check to see if axis exist before getting it's function mapping
    
    - Added dpad to standard directional input
    
    - Fixed keys pressed before game start having an effect on the game
    
    - pom version update

--- src/com/zalinius/zje/utilities/input/GamepadKeyboardDirection.java
+++ src/com/zalinius/zje/utilities/input/GamepadKeyboardDirection.java
@@ -15,14 +15,16 @@ public class GamepadKeyboardDirection implements Directional {
 	
 	private KeyboardDirection wasdDirection;
 	private KeyboardDirection arrowsDirection;
-	private GamepadDirection  gamepadDirection;
+	private GamepadDirection  joyStickDirection;
+	private KeyboardDirection  dPadDirection;
 	
 	public GamepadKeyboardDirection() {
 		ScalingStrategy strategy = ScalingStrategyFactory.clampLengthBetweenZeroAndOne();
 		
 		this.wasdDirection = new KeyboardDirection(BinaryInput.KEY_W, BinaryInput.KEY_A, BinaryInput.KEY_S, BinaryInput.KEY_D, strategy);
 		this.arrowsDirection = new KeyboardDirection(BinaryInput.KEY_UP, BinaryInput.KEY_LEFT, BinaryInput.KEY_DOWN, BinaryInput.KEY_RIGHT, strategy);
-		this.gamepadDirection = new GamepadDirection(strategy);
+		this.joyStickDirection = new GamepadDirection(strategy);
+		this.dPadDirection = new KeyboardDirection(BinaryInput.BUTTON_DPAD_UP, BinaryInput.BUTTON_DPAD_LEFT, BinaryInput.BUTTON_DPAD_DOWN, BinaryInput.BUTTON_DPAD_RIGHT, strategy);
 	}
 
 	@Override
@@ -33,8 +35,11 @@ public class GamepadKeyboardDirection implements Directional {
 		else if(arrowsDirection.direction().isNonZero()) {
 			return arrowsDirection.direction();
 		}
-		else if(gamepadDirection.direction().isNonZero()) {
-			return gamepadDirection.direction();
+		else if(dPadDirection.direction().isNonZero()) {
+			return dPadDirection.direction();
+		}
+		else if(joyStickDirection.direction().isNonZero()) {
+			return joyStickDirection.direction();
 		}
 		else {
 			return new Vector();
@@ -45,11 +50,12 @@ public class GamepadKeyboardDirection implements Directional {
 		List<Inputtable> inputs = new ArrayList<>();
 		inputs.addAll(wasdDirection.inputs());
 		inputs.addAll(arrowsDirection.inputs());
+		inputs.addAll(dPadDirection.inputs());
 		return inputs;
 	}
 	
 	public Collection<Axisable> axisInput() {
-		return gamepadDirection.axisInputs();
+		return joyStickDirection.axisInputs();
 	}
 
 }
---
+++ test/com/zalinius/zje/architecture/input/InputListenerTest.java
@@ -0,0 +1,136 @@
+package com.zalinius.zje.architecture.input;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Canvas;
+import java.awt.event.KeyEvent;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.zalinius.zje.architecture.input.actions.Inputtable;
+import com.zalinius.zje.architecture.input.types.BinaryInput;
+
+public class InputListenerTest {
+	
+	private InputListener inputListener;
+	
+	@BeforeEach
+	void setup() {
+		inputListener = new InputListener();
+	}
+	
+	@Test
+	void keyPressed_whenPressed_activatesPressedCallbackFunction() throws Exception {
+		InputSpy inputSpyA = new InputSpy(BinaryInput.KEY_A);
+		inputListener.addInput(inputSpyA);
+		
+		KeyEvent keyEventA = makeDummyKeyEvent(BinaryInput.KEY_A);
+		inputListener.keyPressed(keyEventA);
+		
+		assertTrue(inputSpyA.wasPressed());
+		assertFalse(inputSpyA.wasReleased());
+	}
+
+	@Test
+	void keyPressAndRelease_whenPressedAndReleased_activatesPressedAndReleasedCallbackFunctions() throws Exception {
+		InputSpy inputSpyA = new InputSpy(BinaryInput.KEY_A);
+		inputListener.addInput(inputSpyA);
+		
+		KeyEvent keyEventA = makeDummyKeyEvent(BinaryInput.KEY_A);
+		inputListener.keyPressed(keyEventA);
+		inputListener.keyReleased(keyEventA);
+		
+		assertTrue(inputSpyA.wasPressed());
+		assertTrue(inputSpyA.wasReleased());
+	}
+
+	@Test
+	void keyRelease_whenKeyOnlyReleased_doesNotActivateReleaseCallbackFunctions() throws Exception {
+		InputSpy inputSpyA = new InputSpy(BinaryInput.KEY_A);
+		inputListener.addInput(inputSpyA);
+		
+		KeyEvent keyEventA = makeDummyKeyEvent(BinaryInput.KEY_A);
+		inputListener.keyReleased(keyEventA);
+		
+		assertFalse(inputSpyA.wasReleased());
+	}
+
+	@Test
+	void keyRelease_whenKeyReleasedThenPressedThenReleased_activatesPressedAndReleasedCallbackFunctionsOnceEach() throws Exception {
+		InputSpy inputSpyA = new InputSpy(BinaryInput.KEY_A);
+		inputListener.addInput(inputSpyA);
+		
+		KeyEvent keyEventA = makeDummyKeyEvent(BinaryInput.KEY_A);
+		inputListener.keyReleased(keyEventA);
+		inputListener.keyPressed(keyEventA);
+		inputListener.keyReleased(keyEventA);
+		
+		assertEquals(1, inputSpyA.getPresses());
+		assertEquals(1, inputSpyA.getReleases());
+	}
+
+	
+
+	
+	private static KeyEvent makeDummyKeyEvent(BinaryInput key) {
+		return new KeyEvent(new Canvas(), 0, 0L, 0, key.getKeyboardCode(),'0',0);
+	}
+	
+	
+	static class InputSpy implements Inputtable{
+		
+		private BinaryInput binaryInput;
+		private int presses;
+		private int releases;
+
+		public InputSpy(BinaryInput binaryInput) {
+			this.binaryInput = binaryInput;
+			this.presses = 0;
+			this.releases = 0;
+		}
+		
+		
+		@Override
+		public void released() {
+			this.releases++;
+		}
+		
+		@Override
+		public void pressed() {
+			this.presses++;			
+		}
+		
+		@Override
+		public BinaryInput binaryInput() {
+			return binaryInput;
+		}
+		
+		
+		public boolean wasPressed() {
+			return presses > 0;
+		}
+
+		public boolean wasReleased() {
+			return releases > 0;
+		}
+
+		public BinaryInput getBinaryInput() {
+			return binaryInput;
+		}
+
+
+		public int getPresses() {
+			return presses;
+		}
+
+
+		public int getReleases() {
+			return releases;
+		}
+		
+		
+
+	}
+
+}
